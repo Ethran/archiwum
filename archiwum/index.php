54cbb58b22b9f9020d2b372a7161e0f141d49ff2
Ethran <wiktorw39@gmail.com>
2023-02-24 01:26:57 +0100

    added blacklist, changed names of folders, improved uploading files

--- archiwum/index.php
+++ archiwum/index.php
@@ -1,10 +1,92 @@
 <?php
+ob_start();
 session_start();
 require 'upload.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+function humanFileSize($size, $unit = "")
+{
+    if ((! $unit && $size >= 1 << 30) || $unit == "GB")
+        return number_format($size / (1 << 30), 2) . "GB";
+    if ((! $unit && $size >= 1 << 20) || $unit == "MB")
+        return number_format($size / (1 << 20), 2) . "MB";
+    if ((! $unit && $size >= 1 << 10) || $unit == "KB")
+        return number_format($size / (1 << 10), 2) . "KB";
+    return number_format($size) . " bytes";
+}
+
+function getFiles($path)
+{
+    // open directory
+    $directory = opendir($path);
+    // get each entry
+    while ($entryName = readdir($directory)) {
+        $items[] = $entryName;
+    }
+    closedir($directory);
+    sort($items);
+    return $items;
+}
+
+function drawTable($path)
+{
+    echo "<table>\n";
+    echo "<thead><tr><th style=\"text-align: right\">Data</th> <th> Zadania: </th> <th>Rozwiązania:</th> <th>Komentarz:</th> </tr> </thead>\n";
+    echo "<tbody>\n";
+
+    // open file list:
+    if (($handle = fopen($path . "/files_informations.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            // todo: wyrzuc error
+            echo "<tr>";
+            echo "<td> $data[0]</td>";
+            echo "<td> <a href=\"$data[1]\"> $data[3]</a></td>";
+            if ($data[2] != "") {
+                echo "<td> <a href=\"$data[2]\"> $data[3] rozw </a></td>";
+            } else {
+                echo "<td> brak</td>";
+            }
+            echo "<td> $data[4]</td>";
+            echo "</tr>";
+        }
+        fclose($handle);
+    }
+    echo "</tbody></table>\n";
+    $nazwa = hash('ripemd160', $path . "/");
+    if (isset($_POST["submit_" . $nazwa])) { // check if form was submitted
+        $result = getFilesUpload($path . "/", $nazwa . "_id", "roz_" . $nazwa . "_id");
+        $_POST = array();
+        if ($result) {
+            header("Location: thank_you");
+        }
+    }
+    echo "<br>";
+    printForm($nazwa);
+}
+
+function wypiszPrzedmiot($pdfDirectory)
+{
+    $year = getFiles($pdfDirectory);
+    echo "<ul class=\"collapse\">\n";
+
+    for ($index = 0; $index < count($year); $index ++) {
+        if (substr("$year[$index]", 0, 1) != "." && is_dir($pdfDirectory . "/" . $year[$index])) {
+            echo "<li>$year[$index]<ul>";
+            $subjects = getFiles($pdfDirectory . "/" . $year[$index]);
+            for ($j = 0; $j < count($subjects); $j ++) {
+                if (substr("$subjects[$j]", 0, 1) != "." && is_dir($pdfDirectory . "/" . $year[$index] . "/" . $subjects[$j])) {
+                    echo "<li>$subjects[$j]<ul>";
+                    drawTable($pdfDirectory . "/" . $year[$index] . "/" . $subjects[$j]);
+                    echo "</ul></li>";
+                }
+            }
+            echo "</ul></li>";
+        }
+    }
+    echo "</ul>";
+}
 
 ?>
 
@@ -12,167 +94,88 @@ error_reporting(E_ALL);
 
 <html lang="pl">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Archiwum egzaminów</title>
-  <meta name="description" content="archiwum egzaminów">
-  <meta name="author" content="Wiktor Wichrowski>
+<title>Archiwum egzaminów</title>
+<meta name="description" content="archiwum egzaminów">
+<meta name="author" content="Wiktor Wichrowski>
 
-  <meta property="og:title" content="Archiwum egzaminów">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://students.mimuw.edu.pl/~ww439108/archiwium/">
-  <meta property="og:description" content="OSI training">
-  <meta property="og:image" content="https://students.mimuw.edu.pl/~ww439108/archiwum/rainbow.png">
+  <meta 
+	
+	
+	property="og:title" content="Archiwum egzaminów">
+<meta property="og:type" content="website">
+<meta property="og:url"
+	content="https://students.mimuw.edu.pl/~ww439108/archiwium/">
+<meta property="og:description" content="OSI training">
+<meta property="og:image"
+	content="https://students.mimuw.edu.pl/~ww439108/archiwum/rainbow.png">
 
-  <link rel="icon" href="https://students.mimuw.edu.pl/~ww439108/archiwum/rainbow.png">
-  <link rel="icon" href="https://students.mimuw.edu.pl/~ww439108/archiwum/penguin.svg" type="image/svg+xml">
-  <link rel="apple-touch-icon" href="https://students.mimuw.edu.pl/~ww439108/archiwium/rainbow.png">
+<link rel="icon"
+	href="https://students.mimuw.edu.pl/~ww439108/archiwum/rainbow.png">
+<link rel="icon"
+	href="https://students.mimuw.edu.pl/~ww439108/archiwum/penguin.svg"
+	type="image/svg+xml">
+<link rel="apple-touch-icon"
+	href="https://students.mimuw.edu.pl/~ww439108/archiwium/rainbow.png">
 
-  <link rel="stylesheet" href="https://students.mimuw.edu.pl/~ww439108/archiwum/css/styles.css?v=1.0">
-  <link rel="stylesheet" href="https://students.mimuw.edu.pl/~ww439108/archiwum/css/styles_collapsible.css?v=1.0">
-  <script src="js/collapse-list.js"></script>
-  <script src="js/file-chooser.js"></script>
+<link rel="stylesheet"
+	href="https://students.mimuw.edu.pl/~ww439108/archiwum/css/styles.css?v=1.0">
+<link rel="stylesheet"
+	href="https://students.mimuw.edu.pl/~ww439108/archiwum/css/styles_collapsible.css?v=1.0">
+<script src="js/collapse-list.js"></script>
+<script src="js/file-chooser.js"></script>
 </head>
 
 <body>
-<div id="content">
+	<div id="content">
 
-<div id="title">
-  <h1>
-    <font color="#FF8080">
-      <em>
-        <strong>A</strong>
-      </em>
-    </font>
-    <font color="#8080FF">
-      <em>
-        <strong>R</strong>
-      </em>
-    </font>
-    <font color="#FF8080">
-      <em>
-        <strong>C</strong>
-      </em>
-    </font>
-    <font color="#8080FF">
-      <em>
-        <strong>H</strong>
-      </em>
-    </font>
-    <font color="#FF8080">
-      <em>
-        <strong>I</strong>
-      </em>
-    </font>
-    <font color="#8080FF">
-      <em>
-        <strong>W</strong>
-      </em>
-    </font>
-    <font color="#FF8080">
-      <em>
-        <strong>U</strong>
-      </em>
-    </font>
-    <font color="#8080FF">
-      <em>
-        <strong>M</strong>
-      </em>
-    </font>
-  </h1>
-</div>
+		<div id="title">
+			<h1>
+				<font color="#FF8080"> <em> <strong>A</strong>
+				</em>
+				</font> <font color="#8080FF"> <em> <strong>R</strong>
+				</em>
+				</font> <font color="#FF8080"> <em> <strong>C</strong>
+				</em>
+				</font> <font color="#8080FF"> <em> <strong>H</strong>
+				</em>
+				</font> <font color="#FF8080"> <em> <strong>I</strong>
+				</em>
+				</font> <font color="#8080FF"> <em> <strong>W</strong>
+				</em>
+				</font> <font color="#FF8080"> <em> <strong>U</strong>
+				</em>
+				</font> <font color="#8080FF"> <em> <strong>M</strong>
+				</em>
+				</font>
+			</h1>
+		</div>
 
-<chapter>
-<chapter_title>Matematyka</chapter_title> 
- <normal_text> 
+		<chapter> <chapter_title>Matematyka</chapter_title> <normal_text
+			style="margin-right: 2%; margin-left: 2%"> 
  
 
 <?php
-function humanFileSize($size,$unit="") {
-  if( (!$unit && $size >= 1<<30) || $unit == "GB")
-    return number_format($size/(1<<30),2)."GB";
-  if( (!$unit && $size >= 1<<20) || $unit == "MB")
-    return number_format($size/(1<<20),2)."MB";
-  if( (!$unit && $size >= 1<<10) || $unit == "KB")
-    return number_format($size/(1<<10),2)."KB";
-  return number_format($size)." bytes";
-}
-function getFiles($path) {
-    // open directory 
-    $directory = opendir($path);
-    // get each entry
-    while($entryName = readdir($directory)) {$items[] = $entryName;} closedir($directory);
-    sort($items);
-    return $items;
-}
-function drawTable($path) {
-	echo "<table>\n";
-	echo "<thead><tr><th style=\"text-align: right\">Data</th> <th> Zadania: </th> <th>Rozwiązania:</th> <th>Komentarz:</th> </tr> </thead>\n";
-  	echo "<tbody>\n";
-
-  	//open file list:
-	if (($handle = fopen($path."/files_informations.csv", "r")) !== FALSE) {
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			//todo: wyrzuc error
-			echo "<tr>";
-			echo "<td> $data[0]</td>";
-			echo "<td> <a href=\"$data[1]\"> $data[3]</a></td>";
-			if($data[2]!=""){
-				echo "<td> <a href=\"$data[2]\"> $data[3] rozw </a></td>";
-			}else{
-				echo "<td> brak</td>";
-			}
-			echo "<td> $data[4]</td>";
-			echo "</tr>";
-		}
-		fclose($handle);
-		
-
-	}
-  	echo "</tbody></table>\n";
-	if(isset($_POST['submit'])){ //check if form was submitted
-		$result = getFilesUpload($path."/");
-		$_POST = array();
-		if($result){
-			header("Location: thank_you");
-		}
-
-	}
-	echo "<br>";
-	printForm($path);
-
-}
-
-
-    $pdfDirectory="pdf";
-    $year=getFiles($pdfDirectory);
-    echo "<ul class=\"collapse\">\n";
-
-        for($index=0; $index < count($year); $index++) 
-        {
-            if (substr("$year[$index]", 0, 1) != "." && is_dir($pdfDirectory."/".$year[$index]))
-            {
-	    		echo "<li>$year[$index]<ul>";
-				$subjects=getFiles($pdfDirectory."/".$year[$index]);
-				for($j=0; $j < count($subjects); $j++) 
-       			{
-       				if (substr("$subjects[$j]", 0, 1) != "." && is_dir($pdfDirectory."/".$year[$index]."/".$subjects[$j]))
-            		{
-			    		echo "<li>$subjects[$j]<ul>";
-			    		drawTable($pdfDirectory."/".$year[$index]."/".$subjects[$j]);
-						echo "</ul></li>"; 
-					}
-				}
-            	echo "</ul></li>";         
-        	}
-        }
-    echo "</ul>";
-    ?>
-   
-
-</normal_text> 
-</div>
+wypiszPrzedmiot("Matematyka");
+?>
+</normal_text> <chapter> <chapter_title>Informatyka</chapter_title> <normal_text
+			style="margin-right: 2%; margin-left: 2%"> 
+<?php
+wypiszPrzedmiot("Informatyka");
+?>
+</normal_text> <chapter> <chapter_title>Bioinformatyka</chapter_title> <normal_text
+			style="margin-right: 2%; margin-left: 2%"> 
+<?php
+wypiszPrzedmiot("Bioinformatyka");
+?>
+</normal_text>
+	
+	</div>
 
 </body>
 </html>
+<?php
+ob_end_flush();
+?>
--- archiwum/upload.php
+++ archiwum/upload.php
@@ -3,138 +3,140 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+function checkString($nazwa)
+{
+    require 'blackList.php';
+    $nazwa = strtolower($nazwa);
+    if (strlen($nazwa) > 100) {
+        echo '<script>alert("twój komentarz jest za długi")</script>';
+        return false;
+    }
+    $reg = '~\b' . implode('\b|\b', $blackList) . '\b~';
 
-	function uploadFile($file,$rozw, $target_dir ) {
-		 if($rozw)
-		 {
-		 	$target_file = $target_dir ."rozw_". basename($_FILES[$file]["name"]);
-		}
-		else
-		{
-			$target_file = $target_dir . basename($_FILES[$file]["name"]);
-		}
-		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $nazwa = strtr($nazwa, "013458", "oieasb");
+    $nazwa = strtr($nazwa, "!$", "is");
 
-		echo "to jest plik: ".$target_file."<br>";
-		// Check if file already exists
-		if (file_exists($target_file)) {
-		  echo "Sorry, file already exists.<br>";
-		  return 0;
-		}
+    preg_match_all($reg, preg_replace('~[.,?! ]~', '', $nazwa), $matches);
 
-		// Check file size
-		echo "rozmiar ".$_FILES[$file]["size"]."<br>";
-		if ($_FILES[$file]["size"] > 1000000000) {
-		  echo "Sorry, your file is too large."."<br>";
-		  return 0;
-		}
-
-		// Allow certain file formats
-		if($imageFileType != "pdf" && $imageFileType != "tar.xz" ) {
-		  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
-		  return 0;
-		}
-		if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
-			echo "The file ". htmlspecialchars( basename( $_FILES[$file]["name"])). " has been uploaded.<br>";
-			return 1;
-	  	}
-		echo "Not uploaded ".$_FILES[$file]["name"]." because of error #".$_FILES[$file]["error"]."<br>";
-		return 0;
-
-	}
-
-function getFilesUpload($target_dir = "uploads/") {
-	echo $target_dir;
-	$tresc="plik_tresc";
-	$rozw="plik_rozwiazania";
-
-	$uploadOk = uploadFile($tresc, false, $target_dir );
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.<br>";
-		return false;
-	}
-
-	
-		//jeśli plik zadań wgrany
-		if (isset($_POST['data_testu'])) {
-			$date = date('Y-m-d', strtotime($_POST['data_testu']));
-		}
-		$path_zad=$target_dir . basename($_FILES[$tresc]["name"]);
-		if (!file_exists($_FILES[$rozw]['tmp_name']) || !is_uploaded_file($_FILES[$rozw]['tmp_name'])) 
-		{
-			$path_rozw="";
-		}
-		else
-		{
-			$path_rozw= $target_dir ."rozw_".basename($_FILES[$tresc]["name"]);
-			$uploadOk = uploadFile($rozw, true, $target_dir );
-		}
-		$target_dir . basename($_FILES[$rozw]["name"]);
-		$informacje= array($date, $path_zad, $path_rozw,$_POST["rodzaj"], $_POST["komentarz"]);
-		if (($handle = fopen($target_dir."files_informations.csv", "a")) !== FALSE) {
-			fputcsv($handle, $informacje);
-			fclose($handle);
-			return true;
-		}
-		else{		
-		return false;
-			echo "AAAAAAAAAAAA";
-		}
-
+    if (count($matches[0]) > 0) {
+        echo '<script>alert("Proszę nie przeklinać/obgadywać dziekana :/")</script>';
+        return false;
+    }
+    return true;
 }
 
-function printForm($nazwa){
-	$nazwa=hash('ripemd160', $nazwa);
-	echo <<< EOT
-	<style>
-	.label {
-	  	color: white;
-	  	padding: 5px;
-		font-family: 'Source Code Pro', monospace;
-		font-family: 'Ubuntu Mono', monospace;	
-	}
-	.label:hover {
-	 	color: white;
-	  	padding: 5px;
-		font-weight: bold;
-		font-family: 'Source Code Pro', monospace;
-		font-family: 'Ubuntu Mono', monospace;	
-	}
+function uploadFile($file, $target_file)
+{
+    // echo "to jest plik: " . $target_file . "<br>";
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo '<script>alert("Sorry, file already exists.")</script>';
+        return false;
+    }
 
-	.info {background-color: #2196F3;} /* Blue */
-	</style>
-	<form action="" method="post" enctype="multipart/form-data">
-		<input type="date" name="data_testu"
-			   value="2023-02-20"
-			   min="2000-01-01" max="2030-12-31">
-		  <select id="rodzaj" name="rodzaj">
-			<option value="Egzamin">Egzamin</option>
-			<option value="Egzamin*">Egzamin*</option>
-			<option value="Kolokwium">Kolokwium</option>
-			<option value="Kolokwium*">Kolokwium*</option>
-		  </select>
-	<input type="file" id="{$nazwa}_id" name="plik_tresc" value="" hidden/>
-	<label for="{$nazwa}_id" class="label info" id="label_{$nazwa}_id">wgraj tresci zadań</label>
-	<input type="file" id="roz_{$nazwa}_id" name="plik_rozwiazania" value="" style="display:none;"/>
-	<label for="roz_{$nazwa}_id" class="label info" id="label_roz_{$nazwa}_id">wgraj rozwiazania zadań</label>
-	<input type="text" name="komentarz" placeholder="Komentarz">
-	<input type="submit" name="submit" value="Save" />
-	</form>
-	
-	<script>
-	const plik_{$nazwa} = document.getElementById('{$nazwa}_id');
-	const rozw_{$nazwa} = document.getElementById('roz_{$nazwa}_id');
-	
-	plik_{$nazwa}.addEventListener('change', function(){
-	  document.getElementById('label_{$nazwa}_id').innerHTML
-		            =  this.files[0].name;
-	})
-	rozw_{$nazwa}.addEventListener('change', function(){
-	  document.getElementById('label_roz_{$nazwa}_id').innerHTML
-		            =  this.files[0].name;
-	})
-	</script>
-	EOT;
+    // Check file size
+    // echo "rozmiar " . $_FILES[$file]["size"] . "<br>";
+    if ($_FILES[$file]["size"] > 1000000000) {
+        echo '<script>alert("Sorry, your file is too large.")</script>';
+        return false;
+    }
+
+    // Allow certain file formats
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    if ($imageFileType != "pdf" && $imageFileType != "xz" && $imageFileType != "zip") {
+        echo '<script>alert("Sorry, only PDF, ZIP, tar.xz files are allowed.")</script>';
+        return false;
+    }
+    if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
+        echo '<script>alert("The file " . htmlspecialchars(basename($_FILES[$file]["name"])) . " has been uploaded.")</script>';
+        return true;
+    }
+    echo '<script>alert("Not uploaded " . $_FILES[$file]["name"] . " because of error #" . $_FILES[$file]["error"])</script>';
+    return false;
+}
+
+function getFilesUpload($target_dir, $tresc, $rozw)
+{
+    if (checkString($_POST["komentarz"]) == false)
+        return false;
+
+    // echo $target_dir;
+    // echo "<br> tresc: ";
+    // echo $tresc; echo "<br> rozw: ";
+    // echo $rozw;
+    if (! file_exists($_FILES[$tresc]['tmp_name']) || ! is_uploaded_file($_FILES[$tresc]['tmp_name'])) {
+        echo '<script>alert("wgraj plik z zadaniami proszę...")</script>';
+        return false;
+    }
+    $path_zad = $target_dir . date("Y-m-d_H-i-s_") . basename($_FILES[$tresc]["name"]);
+    $uploadOk = uploadFile($tresc, $path_zad);
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == false) {
+        return false;
+    }
+
+    // jeśli plik zadań wgrany
+    if (isset($_POST['data_testu'])) {
+        $date = date('Y-m-d', strtotime($_POST['data_testu']));
+    }
+
+    if (! file_exists($_FILES[$rozw]['tmp_name']) || ! is_uploaded_file($_FILES[$rozw]['tmp_name'])) {
+        $path_rozw = "";
+    } else {
+        $path_rozw = $target_dir . date("Y-m-d_H-i-s_") . "rozw_" . basename($_FILES[$rozw]["name"]);
+        $uploadOk = uploadFile($rozw, $path_rozw);
+    }
+    $informacje = array(
+        $date,
+        $path_zad,
+        $path_rozw,
+        $_POST["rodzaj"],
+        $_POST["komentarz"]
+    );
+    if (($handle = fopen($target_dir . "files_informations.csv", "a")) !== FALSE) {
+        fputcsv($handle, $informacje);
+        fclose($handle);
+        return true;
+    } else {
+        echo '<script>alert("AAAAAAAAAAAAA")</script>';
+        return false;
+    }
+}
+
+function printForm($nazwa)
+{
+    echo <<< EOT
+    <form action="" method="post" enctype="multipart/form-data">
+    	<input type="date" name="data_testu"
+    		   value="2023-02-20"
+    		   min="2000-01-01" max="2030-12-31">
+    	  <select id="rodzaj" name="rodzaj">
+    		<option value="Egzamin">Egzamin</option>
+    		<option value="Egzamin*">Egzamin*</option>
+    		<option value="Kolokwium">Kolokwium</option>
+    		<option value="Kolokwium*">Kolokwium*</option>
+    	  </select>
+    <input type="file" id="{$nazwa}_id" name="{$nazwa}_id" value="" hidden/>
+    <label for="{$nazwa}_id" class="label info" id="label_{$nazwa}_id">wgraj tresci zadań</label>
+    <input type="file" id="roz_{$nazwa}_id" name="roz_{$nazwa}_id" value="" style="display:none;"/>
+    <label for="roz_{$nazwa}_id" class="label info" id="label_roz_{$nazwa}_id">wgraj rozwiazania zadań</label>
+    <input type="text" name="komentarz" placeholder="Komentarz">
+    <input type="submit" name="submit_{$nazwa}" value="Save" />
+    </form>
+    
+    <script>
+    const plik_{$nazwa} = document.getElementById('{$nazwa}_id');
+    const rozw_{$nazwa} = document.getElementById('roz_{$nazwa}_id');
+    
+    plik_{$nazwa}.addEventListener('change', function(){
+      document.getElementById('label_{$nazwa}_id').innerHTML
+    	            =  this.files[0].name;
+    })
+    rozw_{$nazwa}.addEventListener('change', function(){
+      document.getElementById('label_roz_{$nazwa}_id').innerHTML
+    	            =  this.files[0].name;
+    })
+    </script>
+    EOT;
 }
 ?>
